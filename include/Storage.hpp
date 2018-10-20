497482d053d67ad779efd6eaad38500900bd42cd
MilkFather <31627231+MilkFather@users.noreply.github.com>
2018-10-20 10:13:22 +0800

    Update Storage.hpp
    The file is now the one on Matrix

--- include/Storage.hpp
+++ include/Storage.hpp
@@ -1,12 +1,12 @@
 #ifndef AGENDA_STORAGE_H
 #define AGENDA_STORAGE_H
 
-#include "Meeting.hpp"
-#include "User.hpp"
 #include <functional>
 #include <list>
 #include <memory>
 #include <string>
+#include "Meeting.hpp"
+#include "User.hpp"
 
 class Storage {
 private:
@@ -18,8 +18,8 @@ private:
   /**
   *   disallow the copy constructor and assign operator
   */
-  Storage(const Storage & t_another) = delete;
-  void operator=(const Storage & t_another) = delete;
+  Storage(const Storage &t_another) = delete;
+  void operator=(const Storage &t_another) = delete;
 
   /**
   *   read file content into memory
@@ -52,7 +52,7 @@ public:
   * create a user
   * @param a user object
   */
-  void createUser(const User & t_user);
+  void createUser(const User &t_user);
 
   /**
   * query users
@@ -81,14 +81,15 @@ public:
   * create a meeting
   * @param a meeting object
   */
-  void createMeeting(const Meeting & t_meeting);
+  void createMeeting(const Meeting &t_meeting);
 
   /**
   * query meetings
   * @param a lambda function as the filter
   * @return a list of fitted meetings
   */
-  std::list<Meeting> queryMeeting(std::function<bool(const Meeting &)> filter) const;
+  std::list<Meeting> queryMeeting(
+      std::function<bool(const Meeting &)> filter) const;
 
   /**
   * update meetings
