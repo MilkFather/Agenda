e359ffeef8652a04f8e8e36a13515f73177ae6f7
MilkFather <31627231+MilkFather@users.noreply.github.com>
2018-10-24 00:06:50 +0800

    Update src/AgendaUI.cpp

--- src/AgendaUI.cpp
+++ src/AgendaUI.cpp
@@ -5,11 +5,35 @@ AgendaUI::AgendaUI() {
 }
 
 void AgendaUI::OperationLoop(void) {
+    do {
+        std::cout << "----------------------- Agenda -------------------------------" << std::endl;
+        std::cout << "Action :" << std::endl;
+        if (this->m_userName == "") {  // not logged in    
+            std::cout << "l   - log in Agenda by user name and password" << std::endl;
+            std::cout << "r   - register an Agenda account" << std::endl;
+            std::cout << "q   - quit Agenda" << std::endl; 
+        } else {  // logged in
+            std::cout << "o   - log out Agenda" << std::endl;
+            std::cout << "dc  - delete Agenda account" << std::endl;
+            std::cout << "lu  - list all Agenda user" << std::endl;
+            std::cout << "cm  - create a meeting" << std::endl;
+            std::cout << "la  - list all meetings" << std::endl;
+            std::cout << "las - list all sponsor meetings" << std::endl;
+            std::cout << "lap - list all participate meetings" << std::endl;
+            std::cout << "qm  - query meeting by title" << std::endl;
+            std::cout << "qt  - query meeting by time interval" << std::endl;
+            std::cout << "dm  - delete meeting by title" << std::endl;
+            std::cout << "da  - delete all meetings" << std::endl;
+        }
+        std::cout << "--------------------------------------------------------------" << std::endl;
 
+        std::string cmd = this->getOperation();
+        bool cont = this->executeOperation(cmd);
+    } while (cont);
 }
 
 void AgendaUI::startAgenda(void) {
-
+    //this->m_agendaService
 }
 
 std::string AgendaUI::getOperation() {
@@ -21,7 +45,7 @@ bool AgendaUI::executeOperation(std::string t_operation) {
 }
 
 void AgendaUI::userLogIn(void) {
-
+    
 }
 
 void AgendaUI::userRegister(void) {
