bc372ce34cec54171e73c70860b4df8551507ceb
MilkFather <31627231+MilkFather@users.noreply.github.com>
2018-10-24 15:34:00 +0800

    Update src/AgendaUI.cpp
    More functions

--- src/AgendaUI.cpp
+++ src/AgendaUI.cpp
@@ -140,41 +140,93 @@ void AgendaUI::deleteUser(void) {
 void AgendaUI::listAllUsers(void) {
     std::cout << "[list all users]" << std::endl;
     std::cout << std::endl;
-    
+    // TODO
 }
 
 void AgendaUI::createMeeting(void) {
+    std::cout << "[create meeting] [the number of participators]" << std::endl;
+    std::cout << "[create meeting] ";
+    int num;
+    std::vector<std::string> parts;
+    std::cin >> num;
+    for (int i = 1; i <= num; i++) {
+        std::cout << "[create meeting] [please enter the participator " << i << " ]" << std::endl;
+        std::cout << "[create meeting] ";
+        std::string name;
+        std::cin >> name;
+        parts.push_back(name);
+    }
+    std::cout << "[create meeting] [title] [start time(yyyy-mm-dd/hh:mm)] [end time(yyyy-mm-dd/hh:mm)]" << std::endl;
+    std::string t, st, et;
+    std::cout << "[create meeting] ";
+    cin >> t >> st >> et;
 
+    if (this->m_agendaService.createMeeting(this->m_userName, t, st, et, parts) == true) {
+        std::cout << "[create meeting] succeed!" << std::endl;
+    } else {
+        std::cout << "[create meeting] error!" << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 void AgendaUI::listAllMeetings(void) {
-
+    std::cout << "[list all meetings]" << std::endl;
+    std::cout << std::endl;
+    this->printMeetings(this->m_agendaService.listAllMeetings(this->m_userName));
+    std::cout << std::endl;
 }
 
 void AgendaUI::listAllSponsorMeetings(void) {
-
+    std::cout << "[list all sponsor meetings]" << std::endl;
+    std::cout << std::endl;
+    this->printMeetings(this->m_agendaService.listAllSponsorMeetings(this->m_userName));
+    std::cout << std::endl;
 }
 
 void AgendaUI::listAllParticipateMeetings(void) {
-
+    std::cout << "[list all participator meetings]" << std::endl;
+    std::cout << std::endl;
+    this->printMeetings(this->m_agendaService.listAllParticipateMeetings(this->m_userName));
+    std::cout << std::endl;
 }
 
 void AgendaUI::queryMeetingByTitle(void) {
-
+    std::cout << "[query meeting] [title]" << std::endl;
+    std::cout << "[query meeting] " << std::endl;
+    std::string t;
+    std::cin >> t;
+    this->printMeetings(this->m_agendaService.meetingQuery(this->m_userName, t));
+    std::cout << std::endl;
 }
 
 void AgendaUI::queryMeetingByTimeInterval(void) {
-
+    std::cout << "[query meetings] [start time(yyyy-mm-dd/hh:mm)] [end time(yyyy-mm-dd/hh:mm)]" << std::endl;
+    std::cout << "[query meetings] ";
+    std::string st, et;
+    std::cin >> st >> et;
+    this->printMeetings(this->m_agendaService.meetingQuery(this->m_userName, st, et));
+    std::cout << std::endl;
 }
 
 void AgendaUI::deleteMeetingByTitle(void) {
-
+    std::cout << "[delete meeting] [title]" << std::endl;
+    std::cout << "[delete meeting] ";
+    string t;
+    std::cin >> t;
+    if (this->m_agendaService.deleteMeeting(this->m_userName, t) == true) {
+        std::cout << "[delete meeting by title] succeed!" << std::endl;
+    } else {
+        std::cout << "[error] delete meeting fail!" << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 void AgendaUI::deleteAllMeetings(void) {
-
+    this->m_agendaService.deleteAllMeetings();
+    std::cout << "[delete all meetings] succeed!" << std::endl;
+    std::cout << std::endl;
 }
 
 void AgendaUI::printMeetings(std::list<Meeting> t_meetings) {
-    
+    // TODO
 }
